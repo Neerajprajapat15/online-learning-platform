8131d1d0731b8a92bd8cb710efeb221e66b5bac7
Neeraj Prajapat <neeraj.prajapat7499@gmail.com>
2024-08-26 15:44:54 +0530

    updated code

--- js/certification.js
+++ js/certification.js
@@ -16,26 +16,6 @@ function Function1(a) {
   }
 
 
-
-
- 
-  
-
-  // window.onclick = function(event) {
-  //   if (!event.target.matches('.dropbtn')) {
-  //     var dropdowns = document.getElementsByClassName("dropdown-content");
-  //     var i;
-  //     for (i = 0; i < dropdowns.length; i++) {
-  //       var openDropdown = dropdowns[i];
-  //       if (openDropdown.classList.contains('show')) {
-  //         openDropdown.classList.remove('show');
-  //       }
-  //     }
-  //   }
-  // }
-
-
-
   // bookmark background
 
   function bookmark_color(a){
